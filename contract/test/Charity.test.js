cc977b0861cf1fa2212c49bca63a1812be939915
advait <advait.deshpande@live.com>
2023-05-20 18:07:11 +0800

    write tests for simple donations and purchase scenario

---
+++ contract/test/Charity.test.js
@@ -0,0 +1,71 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+const { utils } = require("ethers");
+
+describe("Trustless Donation", function () {
+  let donationToken;
+  let donationNFT;
+  let charityFactory;
+  let charity;
+  let supply = ethers.utils.parseEther("1000");
+  
+  beforeEach(async function () {
+    
+    // Deploy contracts for token, NFT, factory, and charity
+
+    const DonationToken = await ethers.getContractFactory("DonationToken");
+    donationToken = await DonationToken.deploy(supply);
+    
+    const DonationNFT = await ethers.getContractFactory("DonationNFT");
+    donationNFT = await DonationNFT.deploy();
+    
+    const CharityFactory = await ethers.getContractFactory("CharityFactory");
+    charityFactory = await CharityFactory.deploy(donationToken.address, donationNFT.address);
+    
+    await donationNFT.setCharityFactory(charityFactory.address);
+
+    // Create a charity contract
+
+    let owner = await charityFactory.owner()
+    await charityFactory.connect(owner).createCharityContract()
+    
+    charity = await charityFactory.connect(owner).charityContracts(owner.address)
+    charity = await ethers.getContractAt("TrustlessDonation", charity)
+  });
+
+  it("Donation", async function () {
+    // Create a donor and transfer tokens to them
+
+    const [_, donor] = await ethers.getSigners();
+    const amount = utils.parseEther("1");
+    await donationToken.transfer(donor.address, amount);
+
+    // Approve the charity contract to spend the donor's tokens
+    await donationToken.connect(donor).approve(charity.address, amount);
+    
+    await expect(charity.connect(donor).donate(amount))
+      .to.emit(charity, "Donation")
+      .withArgs(donor.address, charity.address, amount, 1);
+  });
+
+  it("Purchase", async function () {
+    // Create a donor and supplier and transfer tokens to them
+
+    const [_, donor, supplier] = await ethers.getSigners();
+    const amount = utils.parseEther("1");
+    await donationToken.transfer(donor.address, amount);
+    await donationToken.connect(donor).approve(charity.address, amount);
+    
+    await charity.connect(donor).donate(amount);
+
+    // Register the supplier with the charity contract
+
+    await charity.registerSupplier(supplier.address)
+    await expect(charity.purchase(supplier.address, amount))
+      .to.emit(charity, "Purchase")
+      .withArgs(charity.address, supplier.address, amount);
+  });
+});
