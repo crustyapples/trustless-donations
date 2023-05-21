78839013d748082bcb22378c45f048c979e5a4a6
advait <advait.deshpande@live.com>
2023-05-21 19:36:20 +0800

    Update index.tsx

--- application/pages/index.tsx
+++ application/pages/index.tsx
@@ -1,28 +1,7 @@
-import {
-  useAddress,
-  useUser,
-  useLogin,
-  useLogout,
-  useMetamask,
-} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useState } from "react";
 import Nav from '../components/nav'
 
 const Home: NextPage = () => {
-  const address = useAddress();
-  const connect = useMetamask();
-  const { login } = useLogin();
-  const { logout } = useLogout();
-  const { user, isLoggedIn } = useUser();
-  const [secret, setSecret] = useState();
-
-  const getSecret = async () => {
-    const res = await fetch("/api/secret");
-    const data = await res.json();
-    setSecret(data.message);
-  };
-
   return (
     <div>
       <Nav/>
