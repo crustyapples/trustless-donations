ad2aba9e9edc8c37139e582cdad8580006dfba9c
advait <advait.deshpande@live.com>
2023-06-08 17:06:25 +0800

    Add Confetti for Registration Confirmation

--- application/components/registerCharityButton.tsx
+++ application/components/registerCharityButton.tsx
@@ -1,26 +1,57 @@
 import { Web3Button } from "@thirdweb-dev/react";
+import { useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
 
 interface RegisterCharityProps {
   charityName: string;
 }
 
 const RegisterCharity: React.FC<RegisterCharityProps> = ({ charityName }) => {
+  let enabledStyle = "rounded-lg bg-black text-white p-2 my-2 ml-1 hover:bg-gray-600";
+  let loadingStyle = "rounded-lg bg-gray-600 text-white p-2 my-2 ml-1 hover:bg-gray-600";
   let disabledStyle = "bg-gray-600 text-gray-200 p-2 my-2 ml-1 rounded-lg";
+
+  const [buttonText, setButtonText] = useState("Register");
+  const [buttonStyle, setButtonStyle] = useState(enabledStyle)
+  const [isExploding, setIsExploding] = useState(false);
+
   return (
-    
     <Web3Button
       theme="light"
       contractAddress={process.env.NEXT_PUBLIC_CHARITY_FACTORY!}
-      action={(contract) => {
-        contract.call("createCharityContract", [charityName]).catch((error) => {
-          console.log("user rejected transaction");
-        });
+      
+      onSubmit={() => {
+        setButtonText("Registering");
+        setButtonStyle(loadingStyle);
+      }}
+
+      action={async (contract) => {
+
+        const success = await contract
+          .call("createCharityContract", [charityName])
+          .catch((error) => {
+            console.log("user rejected transaction");
+          });
+
+        if (success) {
+          setButtonText("Registered");
+          setButtonStyle(disabledStyle);
+          setIsExploding(true);
+          setTimeout(() => {
+            setIsExploding(false);
+          }, 1000);
+        }
       }}
       onError={(error) => alert("Something went wrong!")}
-      isDisabled={charityName.length < 1 ? true : false}
-      className={charityName.length < 1 ? disabledStyle : "rounded-lg bg-black text-white p-2 my-2 ml-1 hover:bg-gray-600"}
+      isDisabled={charityName.length < 1 || buttonText == "Registered" ? true : false}
+      className={
+        charityName.length < 1
+          ? disabledStyle
+          : enabledStyle
+      }
     >
-      Register
+      {buttonText}
+      {isExploding && <ConfettiExplosion />}
     </Web3Button>
   );
 };
